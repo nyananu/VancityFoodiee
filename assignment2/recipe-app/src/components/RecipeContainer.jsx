af6bdba4ebd2ce6a555e55ad7948ad026686ea1a
Anusha <asal15@students.cs.ubc.ca>
2022-06-03 00:22:16 -0700

    recipe cards rendering through map and props

--- assignment2/recipe-app/src/components/RecipeContainer.jsx
+++ assignment2/recipe-app/src/components/RecipeContainer.jsx
@@ -2,24 +2,20 @@ import React from "react";
 import RecipeCard from "./RecipeCard"
 import allRecipes from "../allRecipes";
 
+function createRecipeCard(recipe){
+    return(
+        <RecipeCard
+            key = {recipe.id}
+            imgURL = {recipe.imgURL}
+            title = {recipe.title}
+            ingredients = {recipe.ingredients}
+            />
+    );
+}
+
 function RecipeContainer(){
-    console.log(allRecipes);
     return<div className="recipeContainer">
-        <RecipeCard
-            title= {allRecipes[0].title}
-            imgURL={allRecipes[0].imgURL}
-            ingredients= {allRecipes[0].ingredients}
-        />
-        <RecipeCard
-            title= {allRecipes[1].title}
-            imgURL={allRecipes[1].imgURL}
-            ingredients= {allRecipes[1].ingredients}
-        />
-        <RecipeCard
-            title= {allRecipes[2].title}
-            imgURL={allRecipes[2].imgURL}
-            ingredients= {allRecipes[2].ingredients}
-        />
+        {allRecipes.map(createRecipeCard)}
     </div>
 }
 
