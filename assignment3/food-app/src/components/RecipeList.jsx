751bda82f1c1913c05ede368627afa82aa1233a4
Anusha <asal15@students.cs.ubc.ca>
2022-06-15 15:04:55 -0700

    added a3

---
+++ assignment3/food-app/src/components/RecipeList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import SingleRecipe from "./SingleRecipe";
+import {useSelector} from "react-redux";
+
+function RecipeList(){
+    let recipes = useSelector(state => state)
+    return(
+        <div className="recipeContainer">
+            {recipes.map(recipe =>{
+                return <SingleRecipe key={recipe.id}
+                                     imgURL={recipe.imgURL}
+                                     title={recipe.title}
+                                     ingredients={recipe.ingredients}
+                                     instructions={recipe.instructions}
+                />
+            })}
+        </div>
+    )
+}
+
+export default RecipeList;
