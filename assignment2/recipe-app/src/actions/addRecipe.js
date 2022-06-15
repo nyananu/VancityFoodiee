b49904947fbb26f70069834c7fb18f5704c63290
Anusha <asal15@students.cs.ubc.ca>
2022-06-15 14:56:18 -0700

    deleted old a2 directory

--- assignment2/recipe-app/src/actions/addRecipe.js
+++
@@ -1,5 +0,0 @@
-// export const submitRecipe = () => {
-//     return {
-//         type: 'SUBMIT_RECIPE'
-//     }
-// }
--- assignment2/recipe-app/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-// import submitReducer from "./isSubmit";
-// import { combineReducers } from "redux";
-//
-// const rootReducer = combineReducers({
-//     submit : submitReducer
-// })
-//
-// export default rootReducer;
