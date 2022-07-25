4f5f9ca163ce9aad9acc683229fb621f89c51f29
Anusha <asal15@students.cs.ubc.ca>
2022-07-25 01:54:10 -0700

    lets try this

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/api', indexRouter);
+app.use('/api/recipes', recipesRouter);
  if (process.env.NODE_ENV === 'production') {
 //setting up static folder
      app.use(express.static(path.join(__dirname, "food-app", "build")));
@@ -26,7 +28,6 @@ app.use(cookieParser());
 
 
 
-app.use('/api', indexRouter);
-app.use('/api/recipes', recipesRouter);
+
 
 module.exports = app;
