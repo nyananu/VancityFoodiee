e579ff79e05adbf5ddd51a1105aace51bec10a56
Anusha <asal15@students.cs.ubc.ca>
2022-07-24 18:07:47 -0700

    lets try this

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes');
 var recipesRouter = require('./routes/recipes');
 var app = express();
 
@@ -13,7 +13,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+ if (process.env.NODE_ENV === 'production') {
+//setting up static folder
+app.use(express.static(path.join(__dirname, "food-app", "build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "food-app", "build", "index.html"));
+});
+ }
+
+
 
 app.use('/', indexRouter);
 app.use('/recipes', recipesRouter);
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const Recipe = require("../model");
+const Recipe = require("../models/model");
 
 
 /* GET recipes listing. */
