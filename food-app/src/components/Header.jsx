1428457b7d58e105d354e0453aa06699d2818411
Anusha <asal15@students.cs.ubc.ca>
2022-08-25 11:32:41 -0700

    ready for prod

--- food-app/src/components/Header.jsx
+++ food-app/src/components/Header.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import icon from '../images/instagram.svg'
 
 function Header() {
     return <header>
-        <h1>Vancity Foodiee!!!</h1>
+        <h1>
+            Vancity Foodiee
+            <a href='https://www.instagram.com/vancityfoodiee/'
+               target="_blank" rel="noopener noreferrer"
+            >
+            <img className='headerIcon'
+                 src={icon}
+                 alt='IGicon'
+            />
+            </a>
+        </h1>
     </header>
 }
 
